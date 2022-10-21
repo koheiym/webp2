3098390c94693d94676474dd03e1f634ecd7034a
koheiym <91835891+koheiym@users.noreply.github.com>
2022-10-21 23:34:19 +0900

    Add files via upload

--- pra2/scripts/main.js
+++ pra2/scripts/main.js
@@ -1,12 +1,15 @@
 let html=document.getElementById("htmlb");
 let meta=document.getElementById("metab");
 let style=document.getElementById("styleb");
+//const inner_style=document.querySelector("#nav_style");
+//inner_style="<button rel="#style"> style </button>";
 html.onclick=function (){
-    window.scrollTo( 0, html.top);
+    window.scrollTo( 0, 250);
 } 
 meta.onclick=function (){
-    window.scrollTo( 0, meta.top);
+    window.scrollTo( 0, 800);
 }
 style.onclick=function (){
-    window.scrollTo( 0, style.top);
-} 
+    window.scrollTo( 0, 1200);
+}
+
